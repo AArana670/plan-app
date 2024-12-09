28a3c0cbd75d4b2f28dc283d8237c946cec4e677
AArana670 <76699832+AArana670@users.noreply.github.com>
2024-12-09 02:59:43 +0100

    Add users screen

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,25 +1,42 @@
 import React from "react";
 import "../styles/users.css"
 import ProjectHeader from "../components/mainHeader";
+import IconButton from "../components/buttons"
 
-const UserList = ({users}) => {
+const UserList = ({users, roles}) => {
+    var userElems = []
+
+    for (let i in users){
+        userElems.push(
+        <div className="user" id={users[i].name}>
+            {users[i].name}
+            <select className="role-select">
+                {roles.map((role) => {
+                    return <option selected={role==users[i].role}>{role}</option>
+                })}
+            </select>
+        </div>)
+    };
+
     return (
         <div className="userlist">
-            {users}
+            {userElems}
         </div>
     )
 }
 
 const Users = ({id, params}) => {
-    const users = []
+    const users = [{name: "Usuario 1", role: "Transportista"}, {name: "Usuario 2", role: "Transportista"}, {name: "Usuario 3", role: "Administrador"}, {name: "Usuario 1", role: "Transportista"}]
+    const roles = ["Transportista", "Administrador", "Supervisor", "Operario"]
 
     return (
-        <div>
+        <div className="users-main">
             <ProjectHeader id={params.id} current="users"/>
-            <main className="users-main">
-                <UserList users={users}/>
+            <main>
+                <UserList users={users} roles={roles}/>
                 <div className="buttons">
-
+                    <IconButton id="user-add-btn" src="/icons/user-add.svg"/>
+                    <IconButton id="roles-btn" src="/icons/list.svg"/>
                 </div>
             </main>
         </div>
