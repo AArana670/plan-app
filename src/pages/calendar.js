7ebe3d22356e7b8ce90dd9827c5a9ceb6bc1c3b1
AArana670 <76699832+AArana670@users.noreply.github.com>
2025-02-11 22:18:05 +0100

    Fix sidebar style

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "../styles/calendar.css"
 import ProjectHeader from "../components/mainHeader";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 const Calendar = ({params}) => {
     return (
     <div>
         <ProjectHeader id={params.id} current="calendar"/>
         <main className="calendar">
-            <h1>Calendar</h1>
+            <FullCalendar plugins={[ dayGridPlugin ]} initialView="dayGridMonth" />
         </main>
     </div>
     );
